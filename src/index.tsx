830375b1d0dea5fc6531df982cc01750d2122a51
Shashank Vishwakarma <150128068+shashank-vishwakarma-rl@users.noreply.github.com>
2024-05-14 12:29:06 +0530

    Initial setup

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+// @ts-ignore
+import React from 'react'
+// @ts-ignore
+import ReactDOM from 'react-dom/client'
+import App from "./App"
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+)
