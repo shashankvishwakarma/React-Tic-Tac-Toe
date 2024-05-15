ea7354b2c78683f4fbdbd0838ca30dd05891c892
Shashank Vishwakarma <150128068+shashank-vishwakarma-rl@users.noreply.github.com>
2024-05-15 09:47:41 +0530

    initial design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import {useState} from "react";
+
 function App() {
-  return (
-      <h1>React Tic-Tac-Toe</h1>
-  )
+  const [activePlayer, setActivePlayer] = useState('X')
+
+  function handleSelectSquare() {
+    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X')
+  }
+
+  return <main>
+    <div id="game-container">
+      <ol id="players" className="highlight-player">
+        <Player initialName={"Player 1"} symbol={"X"} isActive={activePlayer === 'X'}/>
+        <Player initialName={"Player 2"} symbol={"O"} isActive={activePlayer === 'O'}/>
+      </ol>
+      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+    </div>
+  </main>
 }
 
 export default App
